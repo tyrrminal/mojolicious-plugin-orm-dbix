af0a67387ff1552bd25d046b3fbea8057c3a665b
tyrrminal <mark@tyrrminal.dev>
2024-02-23 17:05:18 -0500

    stub in plugin definition

---
+++ lib/Mojolicious/Plugin/ORM/DBIx.pm
@@ -0,0 +1,16 @@
+package Mojolicious::Plugin::ORM::DBIx;
+use v5.26;
+use warnings;
+
+use Mojo::Base 'Mojolicious::Plugin';
+
+use experimental qw(signatures);
+
+sub register($self, $app, $conf) {
+  
+}
+
+1;
+
+__END__
+
